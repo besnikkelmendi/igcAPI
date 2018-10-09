c67c24001c9f205f3d0db5db3f452b1e984ec50d
besnikk@stud.ntnu.no <35761060+besnikkelmendi@users.noreply.github.com>
2018-10-09 14:14:35 +0200

    v2-Commented

--- igcAPI.go
+++ igcAPI.go
@@ -106,12 +106,12 @@ func main() {
 	//http.HandleFunc("/igcinfo/api/igc/")
 	http.ListenAndServe(":8080", nil)
 
-	s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
+	/*s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
 	track, err := igc.ParseLocation(s)
 	if err != nil {
 		fmt.Errorf("Problem reading the track", err)
 	}
 
 	fmt.Printf("Pilot: %s, gliderType: %s, date: %s",
-		track.Pilot, track.GliderType, track.Date.String())
+		track.Pilot, track.GliderType, track.Date.String())*/
 }
